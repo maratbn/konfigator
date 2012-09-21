065f423df189e8193ef5c5159af0b288611a5b5e
Marat Nepomnyashy <marat@gmail.com>
2012-09-21 02:34:06 -0700

    Moved all the code logic into a class 'Konfigator'.

--- konfigator.py
+++ konfigator.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python
 
-def getCmdArgs():
-    import argparse
+class Konfigator:
 
-    argument_parser = argparse.ArgumentParser(
+    def __init__(self):
+        cmd_args = vars(self.getCmdArgs())
+        print cmd_args
+
+        import os.path
+
+        strAbsPath = os.path.abspath(cmd_args['kernel'])
+
+        print 'abs path:', strAbsPath
+
+
+    def getCmdArgs(self):
+        import argparse
+
+        argument_parser = argparse.ArgumentParser(
                         description='Search the Linux kernel Kconfig files.')
 
-    argument_parser.add_argument(
-        '-k', '--kernel',
-        default='/usr/src/linux/',
-        help='path to the Linux kernel source tree to search (default: %(default)s)',
-        metavar='path')
+        argument_parser.add_argument(
+            '-k', '--kernel',
+            default='/usr/src/linux/',
+            help='path to the Linux kernel source tree to search (default: %(default)s)',
+            metavar='path')
 
-    return argument_parser.parse_args()
+        return argument_parser.parse_args()
 
-cmd_args = vars(getCmdArgs())
-print cmd_args
-
-import os.path
-
-strAbsPath = os.path.abspath(cmd_args['kernel'])
-
-print 'abs path:', strAbsPath
+Konfigator()
