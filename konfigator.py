599dc204692cf53c878c762256345decc06ae71d
Marat Nepomnyashy <marat@gmail.com>
2012-09-21 01:28:42 -0700

    Added basic argument parsing.

--- konfigator.py
+++ konfigator.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 
+import argparse
 
+argument_parser = argparse.ArgumentParser(
+                        description='Search the Linux kernel Kconfig files.')
+
+argument_parser.add_argument(
+    '-k', '--kernel',
+    default='/usr/src/linux/',
+    help='path to the Linux kernel source tree to search (default: %(default)s)',
+    metavar='path')
+
+args = argument_parser.parse_args()
+
+
+print args
