570f778127b9c042a942529b28fd101701a9ae0a
Marat Nepomnyashy <marat@gmail.com>
2012-09-21 02:55:21 -0700

    Eliminated extra lines from inside methods based on a PEP-8 recommendation.

--- konfigator.py
+++ konfigator.py
@@ -6,7 +6,6 @@ class Konfigator:
     def __init__(self):
         self._determineCmdArgs()
         self._determineAbsPathForKernel()
-
         print 'abs path:', self._strAbsPathForKernel
 
 
@@ -26,16 +25,13 @@ class Konfigator:
         '_namespaceCmdArgs' and '_dictCmdArgs'.
         """
         import argparse
-
         argument_parser = argparse.ArgumentParser(
                         description='Search the Linux kernel Kconfig files.')
-
         argument_parser.add_argument(
             '-k', '--kernel',
             default='/usr/src/linux/',
             help='path to the Linux kernel source tree to search (default: %(default)s)',
             metavar='path')
-
         self._namespaceCmdArgs = argument_parser.parse_args()
         self._dictCmdArgs = vars(self._namespaceCmdArgs)
 
