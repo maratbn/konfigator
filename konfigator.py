1f2df7c911a08e3ec4f071b1eac2dc7765ec363c
Marat Nepomnyashy <maratbn@gmail.com>
2012-09-21 20:27:18 -0700

    Added GPL header.

--- konfigator.py
+++ konfigator.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
+#   Konfigator -- Searches the Linux kernel Kconfig files.
+#
+#   Copyright (C) 2012  Marat Nepomnyashy  maratbn@gmail
+#
+#   Licensed under the GNU General Public License Version 3.
+#
+#   This file is part of Konfigator.
+#
+#   Konfigator is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Konfigator is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Konfigator.  If not, see <http://www.gnu.org/licenses/>.
+
 class Konfigator:
 
 
